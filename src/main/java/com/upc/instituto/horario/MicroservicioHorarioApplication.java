108edab69f1c97ef8cfe1ce8dcfd2c41b5fef640
Luis Guisado <lguisadom@gmail.com>
2023-01-30 16:56:53 -0500

    First commit

---
+++ src/main/java/com/upc/instituto/horario/MicroservicioHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.upc.instituto.horario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioHorarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/instituto/horario/MicroservicioHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.instituto.horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
